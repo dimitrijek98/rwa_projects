374b9cfa822b65023ebce0832eb4b2ae327871cf
dkostic98 <dkostic12@gmail.com>
2020-01-19 21:41:17 +0100

    Updates

---
+++ precache-manifest.094e5e3f0a3fe6cdd9039406c2ef9520.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a91f2962d7a9cf6f0611836f1cdd18b",
+    "url": "/rwa/index.html"
+  },
+  {
+    "revision": "da3674cfb53e78a72dc6",
+    "url": "/rwa/static/css/main.d860a3ac.chunk.css"
+  },
+  {
+    "revision": "54859e0f716d534940e4",
+    "url": "/rwa/static/js/2.68e696ec.chunk.js"
+  },
+  {
+    "revision": "da3674cfb53e78a72dc6",
+    "url": "/rwa/static/js/main.76abb348.chunk.js"
+  },
+  {
+    "revision": "968b8f7a535076ac277a",
+    "url": "/rwa/static/js/runtime~main.df639493.js"
+  },
+  {
+    "revision": "d5d4c086a1c84d392a23a14afe673e86",
+    "url": "/rwa/static/media/Anivia_5.d5d4c086.jpg"
+  },
+  {
+    "revision": "cfe946148b10ee0ece2a3077c8ab0ccc",
+    "url": "/rwa/static/media/lolicon.cfe94614.png"
+  }
+]);
